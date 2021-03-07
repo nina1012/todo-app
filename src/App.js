400b3b00ed3d9fabb07265ca630f389809d1df24
nina1012 <labferment10@gmail.com>
2021-03-07 21:13:29 +0100

    adding changes to App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import Hero from './components/Hero/Hero';
+import MainContent from './components/MainContent/MainContent';
 
 function App() {
-  return <div className="App">Hello world</div>;
+  const [darkMode, setDarkMode] = useState(true);
+  const changeMode = () => {
+    setDarkMode(darkMode => !darkMode);
+  };
+
+  useEffect(() => {
+    const color = darkMode ? 'var(--very-dark-blue)' : 'var(--very-light-gra)';
+    document.body.style.backgroundColor = color;
+  }, [darkMode]);
+
+  const mode = {
+    changeMode,
+    darkMode
+  };
+
+  const className = `App ${darkMode ? 'dark' : 'light'}`;
+
+  return (
+    <div className={className}>
+      <Hero {...mode} />
+      <div className="container">
+        <MainContent {...mode} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
